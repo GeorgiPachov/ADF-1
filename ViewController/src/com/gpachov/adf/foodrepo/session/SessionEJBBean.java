878e927433d6f221cd2019d25ccc2c12d4fe9a16
GeorgiPachov <georgi.patchov@gmail.com>
2014-06-13 17:34:17 +0300

    modified some labels

---
+++ ViewController/src/com/gpachov/adf/foodrepo/session/SessionEJBBean.java
@@ -0,0 +1,121 @@
+package com.gpachov.adf.foodrepo.session;
+
+import com.gpachov.adf.foodrepo.ProductEntries;
+import com.gpachov.adf.foodrepo.Products;
+import com.gpachov.adf.foodrepo.Recipes;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import javax.ejb.SessionContext;
+import javax.ejb.Stateful;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
+import javax.persistence.Query;
+
+import oracle.binding.AttributeContext;
+import oracle.binding.RowContext;
+
+@Stateful(name = "SessionEJB", mappedName = "Application1-ViewController-SessionEJB")
+public class SessionEJBBean implements SessionEJB, SessionEJBLocal {
+    @Resource
+    SessionContext sessionContext;
+    @PersistenceContext(unitName = "Persistence", type = PersistenceContextType.EXTENDED)
+    private EntityManager em;
+
+    public SessionEJBBean() {
+    }
+
+    @TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
+    public Object queryByRange(String jpqlStmt, int firstResult, int maxResults) {
+        Query query = em.createQuery(jpqlStmt);
+        if (firstResult > 0) {
+            query = query.setFirstResult(firstResult);
+        }
+        if (maxResults > 0) {
+            query = query.setMaxResults(maxResults);
+        }
+        return query.getResultList();
+    }
+
+    public <T> T persistEntity(T entity) {
+        em.persist(entity);
+        return entity;
+    }
+
+    public <T> T mergeEntity(T entity) {
+        return em.merge(entity);
+    }
+
+    public Products persistProducts(Products products) {
+        em.persist(products);
+        return products;
+    }
+
+    public void removeProducts(Products products) {
+        products = em.find(Products.class, products.getProductId());
+        em.remove(products);
+    }
+
+    /** <code>select o from Products o</code> */
+    @TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
+    public List<Products> getProductsFindAll() {
+        return em.createNamedQuery("Products.findAll", Products.class).getResultList();
+    }
+
+    public ProductEntries persistProductEntries(ProductEntries productEntries) {
+        em.persist(productEntries);
+        return productEntries;
+    }
+
+    public void removeProductEntries(ProductEntries productEntries) {
+        productEntries = em.find(ProductEntries.class, productEntries.getProductEntryId());
+        em.remove(productEntries);
+    }
+
+    /** <code>select o from ProductEntries o</code> */
+    @TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
+    public List<ProductEntries> getProductEntriesFindAll() {
+        return em.createNamedQuery("ProductEntries.findAll", ProductEntries.class).getResultList();
+    }
+
+    public Recipes persistRecipes(Recipes recipes) {
+        em.persist(recipes);
+        return recipes;
+    }
+
+    public void removeRecipes(Recipes recipes) {
+        recipes = em.find(Recipes.class, recipes.getRecipeId());
+        em.remove(recipes);
+    }
+
+    /** <code>select o from Recipes o</code> */
+    @TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
+    public List<Recipes> getRecipesFindAll() {
+        return em.createNamedQuery("Recipes.findAll", Recipes.class).getResultList();
+    }
+
+    public boolean setAttributeValue(AttributeContext p0, Object p1) {
+        return false;
+    }
+
+    public Object createRowData(RowContext p0) {
+        return null;
+    }
+
+    public Object registerDataProvider(RowContext p0) {
+        return null;
+    }
+
+    public boolean removeRowData(RowContext p0) {
+        return false;
+    }
+
+    public void validate() {
+    }
+}
---
+++ ViewController/src/com/gpachov/adf/foodrepo/session/SessionEJBLocal.java
@@ -0,0 +1,49 @@
+package com.gpachov.adf.foodrepo.session;
+
+import com.gpachov.adf.foodrepo.ProductEntries;
+import com.gpachov.adf.foodrepo.Products;
+import com.gpachov.adf.foodrepo.Recipes;
+
+import java.util.List;
+
+import javax.ejb.Local;
+
+import oracle.binding.AttributeContext;
+import oracle.binding.RowContext;
+
+@Local
+public interface SessionEJBLocal {
+    Object queryByRange(String jpqlStmt, int firstResult, int maxResults);
+
+    <T> T persistEntity(T entity);
+
+    <T> T mergeEntity(T entity);
+
+    Products persistProducts(Products products);
+
+    void removeProducts(Products products);
+
+    List<Products> getProductsFindAll();
+
+    ProductEntries persistProductEntries(ProductEntries productEntries);
+
+    void removeProductEntries(ProductEntries productEntries);
+
+    List<ProductEntries> getProductEntriesFindAll();
+
+    Recipes persistRecipes(Recipes recipes);
+
+    void removeRecipes(Recipes recipes);
+
+    List<Recipes> getRecipesFindAll();
+
+    boolean setAttributeValue(AttributeContext p0, Object p1);
+
+    Object createRowData(RowContext p0);
+
+    Object registerDataProvider(RowContext p0);
+
+    boolean removeRowData(RowContext p0);
+
+    void validate();
+}
